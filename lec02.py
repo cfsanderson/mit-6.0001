5b4f9f7caf4e496d98c9fa26b705484b9f8b2beb
cfsanderson <calebsanderson@gmail.com>
2017-11-22 17:08:56 -0500

    All lecture notes and code samples for the MIT Intro to CS and Programming in Python.

---
+++ lec02.py
@@ -0,0 +1,46 @@
+###################
+## EXAMPLE: strings 
+###################
+#hi = "hello there"
+#name = "ana"
+#greet = hi + name  
+#print(greet)
+#greeting = hi + " " + name
+#print(greeting)
+#silly = hi + (" " + name)*3
+#print(silly)
+
+####################
+## EXAMPLE: output 
+####################
+#x = 1
+#print(x)
+#x_str = str(x)
+#print("my fav number is", x, ".", "x=", x)
+#print("my fav number is", x_str + "." + "x=" + x_str)
+#print("my fav number is" + x_str + "." + "x=" + x_str)
+
+
+####################
+## EXAMPLE: input
+####################
+#text = input("Type anything... ")
+#print(5*text)
+#num = int(input("Type a number... "))
+#print(5*num)
+
+
+####################
+## EXAMPLE: conditionals/branching 
+####################
+#x = float(input("Enter a number for x: "))
+#y = float(input("Enter a number for y: "))
+#if x == y:
+#    print("x and y are equal")
+#    if y != 0:
+#        print("therefore, x / y is", x/y)
+#elif x < y:
+#    print("x is smaller")
+#elif x > y:
+#    print("y is smaller")
+#print("thanks!")
